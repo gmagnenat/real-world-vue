113681402d9fb2d273ca9ad41adca201ffb0fef0
G. Mag <g.magnenat@gmail.com>
2025-03-30 10:33:16 +0200

    Add not found view

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 import EventListView from '../views/EventListView.vue';
 import EventDetailsView from '../views/EventDetailsView.vue';
 import AboutView from '../views/AboutView.vue';
+import NotFoundView from '../views/NotFoundView.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFoundView,
+    },
     {
       path: '/',
       name: 'event-list',
